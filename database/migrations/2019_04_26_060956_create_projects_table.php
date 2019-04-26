2b78506db8c5b32523b002c1f0766f701af7e7e2
ashish4591 <ashish@xenium.in>
2019-04-26 11:59:47 +0530

    Create project table

---
+++ database/migrations/2019_04_26_060956_create_projects_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('project_id');
+            $table->string('project_name')->unique();
+            $table->string('projects_type')->nullable();
+            $table->string('project_total_value')->nullable();
+            $table->string('project_start_date');
+            $table->string('project_end_date');
+            $table->string('project_duration');
+            $table->string('project_created_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
